930994d44f1c749d29cac532364d2ad2a29d80db
Rotem Meiri <rotemx@users.noreply.github.com>
2022-06-29 19:07:24 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {FormsModule}        from "@angular/forms";
+import { LayoutComponent } from './components/layout/layout.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    SideBarComponent,
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
